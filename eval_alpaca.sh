3671c9ada8c720ebc66ddfbefc5e76a843da4ee6
simonucl <simon011130@gmail.com>
2024-09-09 18:39:57 -0400

    add eval alpaca

--- eval_alpaca.sh
+++ eval_alpaca.sh
@@ -8,6 +8,16 @@ CHECKPOINT_PATHS=(
     simonycl/llama-3.1-8b-instruct-armorm-iter0
 )
 
+wait_for_server() {
+  # wait for vllm server to start
+  # return 1 if vllm server crashes
+  local api_key=token-abc123
+  timeout 1200 bash -c '
+    until curl -X POST -H "Authorization: Bearer '"$api_key"'" localhost:8000/v1/completions; do
+      sleep 1
+    done' && return 0 || return 1
+}
+
 NUM_GPUS=2
 SLEEP=240
 for CHECKPOINT_PATH in "${CHECKPOINT_PATHS[@]}"; do
@@ -18,12 +28,20 @@ for CHECKPOINT_PATH in "${CHECKPOINT_PATHS[@]}"; do
         --bench_name alpaca_eval
 
     # Step 2: Start vllm server (TODO: have to be in the background and wait for it to be ready, kill it after eval)
-    python3 -m vllm.entrypoints.openai.api_server --model $CHECKPOINT_PATH --dtype auto --api-key token-abc123 --port 8000 --tensor-parallel-size $NUM_GPUS > vllm.log &
-
+    python3 -m vllm.entrypoints.openai.api_server --model $CHECKPOINT_PATH --dtype auto --api-key token-abc123 --port 8000 --tensor-parallel-size $NUM_GPUS > vllm.log 2>&1 &
+    # python3 -m vllm.entrypoints.openai.api_server --model simonycl/llama-3.1-8b-instruct-armorm-iter0 --dtype auto --api-key token-abc123 --port 8000 --tensor-parallel-size 2 > vllm.log 2>&1 &
+    
     # python -m sglang.launch_server --model-path $CHECKPOINT_PATH --api-key token-abc123 --port 8000 --dp 4 > sglang.log &
 
     # Wait for the server to be ready
-    sleep $SLEEP
+    if ! wait_for_server; then
+        echo "VLLM server failed to start or crashed. Skipping this model."
+        pkill -f vllm
+        pkill -f multiprocessing
+        pkill -f sglang
+        continue
+    echo "VLLM server started successfully for $MODEL_NAME"
+    fi
 
     # Step 3: Run gen answer
     # python3 gen_answer.py \
