2c56242ed05796ed32fb455507ce1bc2ea7e0600
simonucl <simon011130@gmail.com>
2024-09-29 19:28:14 -0400

    add gen by hf

--- eval.sh
+++ eval.sh
@@ -1,5 +1,6 @@
 CHECKPOINT_PATHS=(
-    /mnt/nfs/public/hf/models/meta-llama/Meta-Llama-3.1-8B-Instruct
+    ChenmieNLP/Llama-3-8B-DPO-Iter6
+    
 )
 
 wait_for_server() {
@@ -20,24 +21,24 @@ for CHECKPOINT_PATH in "${CHECKPOINT_PATHS[@]}"; do
     python3 gen_config.py \
         --model_path $CHECKPOINT_PATH
 
-    # Step 2: Start vllm/sglang server
-    if [ "$INFERENCE" == "vllm" ]; then
-        python3 -m vllm.entrypoints.openai.api_server --model $CHECKPOINT_PATH --dtype auto --api-key token-abc123 --port 8000 --tensor-parallel-size $NUM_GPUS > vllm.log 2>&1 &    
-    elif [ "$INFERENCE" == "sglang" ]; then
-        python3 -m sglang.launch_server --model-path $CHECKPOINT_PATH --api-key token-abc123 --port 8000 --dp $NUM_GPUS > sglang.log 2>&1 &
-    fi
+    # # Step 2: Start vllm/sglang server
+    # if [ "$INFERENCE" == "vllm" ]; then
+    #     python3 -m vllm.entrypoints.openai.api_server --model $CHECKPOINT_PATH --dtype auto --api-key token-abc123 --port 8000 --tensor-parallel-size $NUM_GPUS > vllm.log 2>&1 &    
+    # elif [ "$INFERENCE" == "sglang" ]; then
+    #     python3 -m sglang.launch_server --model-path $CHECKPOINT_PATH --api-key token-abc123 --port 8000 --dp $NUM_GPUS > sglang.log 2>&1 &
+    # fi
 
     # Wait for the server to be ready
-    if ! wait_for_server; then
-        echo "VLLM server failed to start or crashed. Exiting."
-        pkill -f multiprocessing
-        pkill -f $INFERENCE
-        exit 1
-    echo "VLLM server started successfully for $MODEL_NAME"
-    fi
+    # if ! wait_for_server; then
+    #     echo "VLLM server failed to start or crashed. Exiting."
+    #     pkill -f multiprocessing
+    #     pkill -f $INFERENCE
+    #     exit 1
+    # echo "VLLM server started successfully for $MODEL_NAME"
+    # fi
 
     # Step 3: Run gen answer
-    python3 gen_answer.py \
+    python3 gen_answer_hf.py \
         --setting-file config/$MODEL_NAME/gen_answer_config.yaml \
         --endpoint-file config/$MODEL_NAME/api_config.yaml
 
@@ -49,15 +50,15 @@ for CHECKPOINT_PATH in "${CHECKPOINT_PATHS[@]}"; do
     #     --use_vllm_server
 
     # alpaca_eval --model_outputs results/alpaca_eval/${MODEL_NAME}/${MODEL_NAME}-greedy-long-output.json
-    
-    # Step 4: Run gen judgement
-    python3 gen_judgement.py \
-        --setting-file config/$MODEL_NAME/judge_config.yaml \
-        --endpoint-file config/$MODEL_NAME/api_config.yaml
 
-    # Step 5: Kill vllm server by port and kill all with name ray
-    pkill -f multiprocessing
-    pkill -f $INFERENCE
+    # Step 4: Run gen judgement
+    # python3 gen_judgement.py \
+    #     --setting-file config/$MODEL_NAME/judge_config.yaml \
+    #     --endpoint-file config/$MODEL_NAME/api_config.yaml
+
+    # # Step 5: Kill vllm server by port and kill all with name ray
+    # pkill -f multiprocessing
+    # pkill -f $INFERENCE
 done
 
 python3 show_result.py
---
+++ gen_answer_hf.py
@@ -0,0 +1,210 @@
+"""Generate answers using api endpoints.
+
+Usage:
+python gen_api_answer --parallel 32
+"""
+import argparse
+import json
+import os
+import re
+import time
+import concurrent.futures
+
+import tiktoken
+import shortuuid
+import tqdm
+import torch
+import transformers
+import tqdm
+
+from add_markdown_info import count_markdown_elements, remove_pattern
+from utils import (
+    load_questions,
+    load_model_answers,
+    make_config,
+    get_endpoint,
+    chat_completion_openai,
+    chat_completion_anthropic,
+    chat_completion_openai_azure,
+    chat_completion_mistral,
+    http_completion_gemini,
+    chat_completion_cohere,
+    reorg_answer_file,
+    OPENAI_MODEL_LIST,
+    temperature_config,
+)
+
+
+def get_answer(
+    question: dict, model: str, endpoint_info: dict, num_choices: int, max_tokens: int, temperature: float, answer_file: str, api_dict: dict
+):
+    if question["category"] in temperature_config:
+        temperature = temperature_config[question["category"]]
+
+    api_type = endpoint_info["api_type"]
+
+    conv = []
+
+    if "system_prompt" in endpoint_info.keys():
+        conv.append({"role": "system", "content": endpoint_info["system_prompt"]})
+    elif model in OPENAI_MODEL_LIST:
+        conv.append({"role": "system", "content": "You are a helpful assistant."})
+
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    choices = []
+    for i in range(num_choices):
+        turns = []
+        for j in range(len(question["turns"])):
+            conv.append({"role": "user", "content": question["turns"][j]["content"]})
+            if api_type == "anthropic":
+                output = chat_completion_anthropic(model=endpoint_info["model_name"],
+                                                   messages=conv,
+                                                   temperature=temperature,
+                                                   max_tokens=max_tokens)
+            elif api_type == "mistral":
+                output = chat_completion_mistral(model=endpoint_info["model_name"],
+                                                 messages=conv,
+                                                 temperature=temperature,
+                                                 max_tokens=max_tokens)
+            elif api_type == "gemini":
+                output = http_completion_gemini(model=endpoint_info["model_name"],
+                                                message=question["turns"][j]["content"],
+                                                temperature=temperature,
+                                                max_tokens=max_tokens)
+            elif api_type == "azure":
+                output = chat_completion_openai_azure(model=endpoint_info["model_name"],
+                                                      messages=conv,
+                                                      temperature=temperature,
+                                                      max_tokens=max_tokens,
+                                                      api_dict=api_dict)
+            elif api_type == "cohere":
+                output = chat_completion_cohere(model=endpoint_info["model_name"],
+                                                messages=conv,
+                                                temperature=temperature,
+                                                max_tokens=max_tokens)
+            else:
+                output = chat_completion_openai(model=endpoint_info["model_name"], 
+                                                messages=conv, 
+                                                temperature=temperature, 
+                                                max_tokens=max_tokens, 
+                                                api_dict=api_dict)
+            conv.append({"role": "assistant", "content": output})
+
+            turns.append({"content": output})
+        choices.append({"index": i, "turns": turns})
+    
+    # Dump answers
+    ans = {
+        "question_id": question["question_id"],
+        "answer_id": shortuuid.uuid(),
+        "model_id": model,
+        "choices": choices,
+        "tstamp": time.time(),
+    }
+    
+    if len(choices) == len(turns) == 1:
+        metadata = {"token_len": len(encoding.encode(output, 
+                                                     disallowed_special=()))}
+        ans["conv_metadata"] = metadata | count_markdown_elements(remove_pattern(output, 
+                                                                     re.compile("```([^`]*)```")),
+                                                                 suffix="")
+
+    os.makedirs(os.path.dirname(answer_file), exist_ok=True)
+    with open(answer_file, "a") as fout:
+        fout.write(json.dumps(ans) + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--setting-file", type=str, default="config/gen_answer_config.yaml"
+    )
+    parser.add_argument(
+        "--endpoint-file", type=str, default="config/api_config.yaml"
+    )
+    args = parser.parse_args()
+
+    settings = make_config(args.setting_file)
+    endpoint_list = make_config(args.endpoint_file)
+
+    existing_answer = {}
+    
+    print(settings)
+
+    for model in settings["model_list"]:
+        assert model in endpoint_list
+        endpoint_info = endpoint_list[model]
+
+        question_file = os.path.join("data", settings["bench_name"], "question.jsonl")
+        questions = load_questions(question_file)
+
+        answer_file = os.path.join("data", settings["bench_name"], "model_answer", f"{model}.jsonl")
+        print(f"Output to {answer_file}")
+
+        if "parallel" in endpoint_info:
+            parallel = endpoint_info["parallel"]
+        else:
+            parallel = 1
+
+        max_tokens = settings["max_tokens"]
+
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+        tokenizer = AutoTokenizer.from_pretrained(endpoint_info["tokenizer"])
+        model_kwargs = {
+            "trust_remote_code": True,
+            "torch_dtype": torch.bfloat16,
+            "attn_implementation": "flash_attention_2",
+            "device": "cuda"
+        }
+        
+        model = AutoModelForCausalLM.from_pretrained(endpoint_info["model_name"], **model_kwargs)
+        convs = [
+            {
+                "role": "user",
+                "content": question["turns"][0]["content"]
+            }
+            for question in questions
+        ]
+        tokenized_convs = [
+            tokenizer(tokenizer.apply_chat_template(conv, tokenize=False, add_generation_prompt=True), return_tensors="pt").to("cuda")
+            for conv in convs
+        ]
+        results = []
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        with torch.no_grad():
+            for i, tokenized_conv in tqdm.tqdm(enumerate(tokenized_convs), total=len(tokenized_convs)):
+                turns = []
+                outputs = model.generate(
+                    input_ids=tokenized_conv.input_ids,
+                    attention_mask=tokenized_conv.attention_mask,
+                    max_new_tokens=max_tokens,
+                    temperature=settings["temperature"],
+                )
+                output = tokenizer.decode(outputs[0][len(tokenized_conv.input_ids[0]):])
+                turns.append({"content": output})
+                choices = [
+                    {
+                        "index": i,
+                        "turns": turns
+                    }
+                ]
+                ans = {
+                    "question_id": questions[i]["question_id"],
+                    "answer_id": shortuuid.uuid(),
+                    "model_id": model,
+                    "choices": choices,
+                    "tstamp": time.time(),
+                }
+                if len(choices) == len(turns) == 1:
+                    metadata = {"token_len": len(encoding.encode(output, 
+                                                     disallowed_special=()))}
+                    ans["conv_metadata"] = metadata | count_markdown_elements(remove_pattern(output, 
+                                                                     re.compile("```([^`]*)```")),
+                                                                 suffix="")
+                results.append(ans)
+        os.makedirs(os.pathdirname(answer_file), exist_ok=True)
+        with open(answer_file, "w") as fout:
+            for result in results:
+                fout.write(json.dumps(result) + "\n")
+
+        reorg_answer_file(answer_file)
